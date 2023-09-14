7a3525ecc78398afa4c8c2aae16801523f5109a7
sarbajits <141050766+sarbajits@users.noreply.github.com>
2023-09-14 14:36:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+const excel = document.getElementById('excel')
+const main = document.querySelector(".main")
+excel.innerHTML = `
+<img src="https://media.proprofs.com/images/QM/user_images/2503852/New%20Project%20-%202021-02-12T211518_067.jpg" alt="Excel">
+<h3>Excel Note</h3>
+`;
+excel.addEventListener('click',
+    function (e) {
+        console.log("Excel");
+        main.innerHTML = `
+<iframe src="https://drive.google.com/file/d/1FIvBDIlI9Gu5FFFU8co4nAJkMFeW17p9/preview" width="100%" height="100%" allow="autoplay"></iframe>
+    `;
+    })
