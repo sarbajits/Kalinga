a404c0dee1efc39873c1299f672a136e68e75841
sarbajits <sarbajitsahoo8260@gmail.com>
2023-09-18 22:57:17 +0530

    Toppers Updated

--- scripts/sliding_script.js
+++ scripts/sliding_script.js
@@ -1,3 +1,50 @@
+const studentsData = [
+  { image: '/images/toppers/st1.jpg', name: 'Arpita' },
+  { image: '/images/toppers/st2.jpg', name: 'Suchismita' },
+  { image: '/images/toppers/st3.jpg', name: 'Gayatree' },
+  { image: '/images/toppers/st4.jpg', name: 'Student 4' },
+  { image: '/images/toppers/st5.jpg', name: 'Student 5' },
+  { image: '/images/toppers/st6.jpg', name: 'Student 6' }
+];
+
+const topperSection = document.getElementById('topper-section');
+
+let currentIndex = 0;
+
+function updateStudents() {
+  topperSection.innerHTML = '';
+
+  for (let i = currentIndex; i < currentIndex + 3; i++) {
+    const student = studentsData[i % studentsData.length];
+    const studentDiv = document.createElement('div');
+    studentDiv.classList.add('student');
+
+    const image = document.createElement('img');
+    image.src = student.image;
+    image.alt = student.name;
+    image.classList.add('circle-image');
+
+    const nameDiv = document.createElement('div');
+    nameDiv.textContent = student.name;
+    nameDiv.classList.add('student-name', 'blink');
+
+    studentDiv.appendChild(image);
+    studentDiv.appendChild(nameDiv);
+
+    topperSection.appendChild(studentDiv);
+  }
+}
+
+function rotateStudents() {
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % studentsData.length;
+    updateStudents();
+  }, 2000);
+}
+
+updateStudents();
+rotateStudents();
+
 let slideIndex = 0;
 showSlides();
 
