2429af845ca1f817e5fd6634b91bdd22a1f47ce1
sarbajits <sarbajitsahoo8260@gmail.com>
2023-09-20 10:47:49 +0530

    Slides

--- scripts/sliding_script.js
+++ scripts/sliding_script.js
@@ -7,7 +7,6 @@ const studentsData = [
   { image: '/images/toppers/st6.jpg', name: 'Chinmayee' }
 ];
 
-
 const topperSection = document.getElementById('topper-section');
 
 let currentIndex = 0;
@@ -53,3 +52,23 @@ function rotateStudents() {
 preloadImages();  // Preload images before starting rotation
 updateStudents();
 rotateStudents();
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
