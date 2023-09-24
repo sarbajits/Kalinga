de667590e4f942df11f1f2be4bb91c76a821fecc
sarbajits <sarbajitsahoo8260@gmail.com>
2023-09-24 16:06:22 +0530

    Exam Updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 var users = [
     { username: "admin", password:"kce@2006", name: "Kalinga Computer" },
-    { username: staff", password: "kalinga", name: "Kalinga Staff" },
+    { username: "staff", password: "kalinga", name: "Kalinga Staff" },
     {
         "name": "Sarbajit Sahoo",
         "username": "Sarbajit",
