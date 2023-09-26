7ee097bc79649275650aed80dbcae2e18be94cdc
sarbajits <141050766+sarbajits@users.noreply.github.com>
2023-09-26 08:37:35 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 var users = [
     { username: "admin", password:"kce@2006", name: "Kalinga Computer" },
     { username: "staff", password: "kalinga", name: "Kalinga Staff" },
+    { username:"temp", password: "kalinga", name: "Kalinga Staff" },
     {
         "name": "Sarbajit Sahoo",
         "username": "Sarbajit",
