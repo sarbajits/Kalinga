122d82d67dd59a40238c6a06cffbff8df6cf52aa
sarbajits <sarbajitsahoo8260@gmail.com>
2023-10-09 16:31:23 +0530

    Build lock system in exam

---
+++ scripts/lock.js
@@ -0,0 +1,40 @@
+// var iframe = document.getElementById('form_google');
+
+// function reloadIframe() {
+//     iframe.src = iframe.src;  // Reload the iframe
+// }
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        lock();
+    }
+});
+
+function checkPasswordAndRemoveOverlay() {
+    const password = document.getElementById('passwordInput').value;
+
+    // Replace 'your_password' with your actual password
+    if (password === '00') {
+        document.getElementById('overlay').style.zIndex = -100;
+    } else {
+        // alert('Incorrect password. Please try again.');
+    }
+}
+
+function checkPasswordAndRefresh() {
+    const password = document.getElementById('passwordInput').value;
+
+    // Replace 'your_password' with your actual password
+    if (password === '00') {
+        location.reload();
+        password === "";
+    } else {
+        // alert('Incorrect password. Please try again.');
+    }
+}
+
+function lock() {
+    document.getElementById('passwordInput').value="";
+    document.getElementById('overlay').style.zIndex = 999;
+}
+
+// lock();
